2d550162ccc447fa4f14ac5fa2a3a8a769d32444
Glitch (hello-express) <none>
2020-04-26 08:43:36 +0000

    🖖👵 Checkpoint ./db.json:15273419/8 ./server.js:15273419/119 ./views/index.pug:15273419/98

--- server.js
+++ server.js
@@ -27,6 +27,11 @@ app.get("/books", (req, res) => {
   res.render("books");
 });
 
+app.get("/books/:id",(req,res)=>{
+  var id = req.params.id
+  db.get("books").remove({id: id});
+})
+
 app.post("/books", (req, res) => {
   req.body.id = shortid.generate();
   db.get("books")
