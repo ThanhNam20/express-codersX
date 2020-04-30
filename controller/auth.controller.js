a9669ee3d81239c39677f588d4259cffcce427ac
Glitch (hello-express) <none>
2020-04-30 09:15:23 +0000

    ✈️🐥 Checkpoint ./views/auth/login.pug:15273419/954 ./views/books/create.pug:15273419/18 ./controller/auth.controller.js:15273419/398

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -3,3 +3,19 @@ var db = require("../db");
 module.exports.login = (req,res)=>{
   res.render('auth/login');
 }
+
+module.exports.postLogin = (req,res)=>{
+  var email = req.body.email;
+  var password = req.body.password;
+  var user = db.get('users').find({email:email}).value();
+  if(!user){
+    res.render('auth/login',{
+      errors:[
+        'User is not found!'
+      ]
+    });
+  }
+  if(!user.password){
+    res
+  }
+}
