8892427ccc1a7cb5ed6b505c3ca090d6b0c8805f
Glitch (hello-express) <none>
2020-04-30 17:15:23 +0000

    🧀🍴 Checkpoint ./middleware/isAdmin.js:15273419/630 ./server.js:15273419/221 ./views/layouts/navbar.pug:15273419/22 ./views/index.pug:15273419/22

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,9 +1,10 @@
 var db = require('../db');
-module.exports.isAdmin = (req,res,next)=>{
-  var user = db.get('users').find({ id: req.cookies.userId }).value();
-  if(user.isAdmin === true){
-      res.redirect('/transactions')
-    }else{
-      res.redirect('/');
+module.exports.isAdmin = (isAdmin) =>{
+    return function (req, res, next){
+        let user = db.get("users").find({id: req.cookies.userId1}).value();
+        if (user.isAdmin === isAdmin){
+            res.redirect('/transactions')
+            next();
+        } else res.redirect('/books');
     }
 }
