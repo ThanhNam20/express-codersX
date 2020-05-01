be2d081becd4d76536d8a10bb4f4ace1fccebf13
Glitch (hello-express) <none>
2020-05-01 08:34:08 +0000

    📯📷 Checkpoint ./controller/auth.controller.js:15273419/2562 ./routes/auth.route.js:15273419/327 ./db.json:15273419/1398

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,57 +1,52 @@
 var db = require("../db");
 const shortid = require("shortid");
-var md5= require("md5");
-const bcrypt = require('bcrypt');
+var md5 = require("md5");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-module.exports.register = (req,res,next)=>{
-  res.render('auth/register');
-}
+module.exports.register = (req, res, next) => {
+  res.render("auth/register");
+};
 
-module.exports.saveRegister = (req,res,next)=>{
-  bcrypt.hash(req.body.passwordsignup, saltRounds, function (err, hash) {
-   db.User.create({
-   name: req.body.name,
-   email: req.body.email,
-   password: hash
-   }).then(function(data) {
-    if (data) {
-    res.redirect('/home');
-    }
+module.exports.saveRegister = (req, res, next) => {
+  req.body.id = shortid.generate();
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    db.get("users").push({
+      name: req.body.name,
+      email: req.body.email,
+      password: hash,
+      isAdmin: false
+    }).then(function(data) {
+      if (data) {
+        res.redirect("/auth/login");
+      }
+    });
   });
- });
-});
-  var name = req.body.name;
+};
+
+module.exports.login = (req, res) => {
+  res.render("auth/login");
+};
+
+module.exports.postLogin = (req, res) => {
   var email = req.body.email;
   var password = req.body.password;
-}
-
-
-module.exports.login = (req,res)=>{
-  res.render('auth/login');
-}
-
-module.exports.postLogin = (req,res)=>{
-  var email = req.body.email;
-  var password = req.body.password;
-  var user = db.get('users').find({email:email}).value();
-  if(!user){
-    res.render('auth/login',{
-      errors:[
-        'User is not found!'
-      ]
+  var user = db
+    .get("users")
+    .find({ email: email })
+    .value();
+  if (!user) {
+    res.render("auth/login", {
+      errors: ["User is not found!"]
     });
   }
-  var hashPassword = md5(password)
-  if(user.password !== hashPassword){
-    res.render("auth/login",{
-      errors:[
-        'wrong password!'
-      ]
+  var hashPassword = md5(password);
+  if (user.password !== hashPassword) {
+    res.render("auth/login", {
+      errors: ["wrong password!"]
     });
     return;
   }
-  res.cookie('userId',user.id);
-  res.redirect('/');
-}
-
+  res.cookie("userId", user.id);
+  res.redirect("/");
+};
