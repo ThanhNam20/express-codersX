f6e31fdeb01f6b68a53558c5f19e735648eb800c
Glitch (hello-express) <none>
2020-05-02 13:43:26 +0000

    📚🕋 Checkpoint ./controller/auth.controller.js:15273419/14694 ./db.json:15273419/621

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -3,7 +3,7 @@ const shortid = require("shortid");
 var md5 = require("md5");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
-
+const sgMail = require("@sendgrid/mail");
 module.exports.register = (req, res, next) => {
   res.render("auth/register");
 };
@@ -13,6 +13,7 @@ module.exports.saveRegister = (req, res, next) => {
   bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
     db.get("users")
       .push({
+        wrongLoginCount: 0,
         id: req.body.id,
         name: req.body.name,
         email: req.body.email,
@@ -35,24 +36,50 @@ module.exports.postLogin = (req, res) => {
     .get("users")
     .find({ email: email })
     .value();
+  var wrongLoginCount = user.wrongLoginCount;
   if (!user) {
     res.render("auth/login", {
       errors: ["User is not found!"]
     });
   } else {
     bcrypt.compare(req.body.password, user.password, function(err, result) {
-      if (result == true) {
-        res.redirect("/");
-      } else {
+      if (result == false) {
+        wrongLoginCount++;
+        console.log(wrongLoginCount);
+        db.get("users")
+          .find({ id: user.id })
+          .assign({ wrongLoginCount: wrongLoginCount })
+          .write();
         res.render("auth/login", {
           errors: ["Wrong password!"]
         });
+        if (wrongLoginCount >= 2) {
+          sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+          const msg = {
+            to: "thanhnam2462000@gmail.com",
+            from: "khuatthanhnam@gmail.com",
+            subject: "Sending with Twilio SendGrid is Fun",
+            text: "and easy to do anywhere, even with Node.js",
+            html: "<strong>and easy to do anywhere, even with Node.js</strong>"
+          };
+          //ES6
+          sgMail.send(msg).then(
+            () => {},
+            error => {
+              console.error(error);
+
+              if (error.response) {
+                console.error(error.response.body);
+              }
+            }
+          );
+        }
+      } else {
+        res.cookie("userId", user.id, {
+          signed: true
+        });
+        res.redirect("/");
       }
     });
   }
-
-  res.cookie("userId", user.id, {
-    signed: true
-  });
-  res.redirect("/");
 };
