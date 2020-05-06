de419fb4402b0f8c4cc1cef5658497697ed24362
Glitch (hello-express) <none>
2020-05-06 15:21:03 +0000

    🤘📍 Checkpoint ./middleware/isAdmin.middleware.js:15273419/44 ./views/layouts/common.pug:15273419/24 ./db.json:15273419/9 ./routes/auth.route.js:15273419/48

--- middleware/isAdmin.middleware.js
+++ middleware/isAdmin.middleware.js
@@ -1,9 +1,9 @@
 var db = require('../db');
 module.exports.isAdmin = (isAdmin) =>{
     return function (req, res, next){
-        let user = db.get("users").find({id: req.cookies.userId1}).value();
+        let user = db.get("users").find({id: req.signedCookies.userId}).value();
         if (user.isAdmin === isAdmin){
-            res.redirect('/transactions')
+            res.redirect('/transactions');
             next();
         } else res.redirect('/books');
     }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 var authController = require("../controller/auth.controller.js");
 
+
 router.get('/register',authController.register);
 router.post('/register',authController.saveRegister);
 router.get("/login",authController.login);
 router.post("/login",authController.postLogin);
+
 module.exports = router;
