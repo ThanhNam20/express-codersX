b2fffd5303cc63948d4a8d98c5f1ddabc9aab398
Glitch (hello-express) <none>
2020-05-06 17:41:30 +0000

    🐃🚕 Checkpoint ./routes/user.route.js:15273419/105 ./public/uploads_conflict_cebd91029738:15273419/2 ./controller/user.controller.js:15273419/2668

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,49 +1,58 @@
-var db = require("../db");  
+var db = require("../db");
 const shortid = require("shortid");
 
-
-module.exports.index = (req,res)=>{
-  res.render('users/index',{
+module.exports.index = (req, res) => {
+  res.render("users/index", {
     users: db.get("users").value()
   });
-}
+};
 
-module.exports.detele = (req,res)=>{
+module.exports.detele = (req, res) => {
   var id = req.params.id;
-  db.get("users").remove({id:id}).write();
-  res.redirect('/users')
-}
+  db.get("users")
+    .remove({ id: id })
+    .write();
+  res.redirect("/users");
+};
 
-module.exports.create = (req,res)=>{
-  res.render('users/createUser');
-}
+module.exports.create = (req, res) => {
+  res.render("users/createUser");
+};
 
-module.exports.rename = (req,res)=>{
-  var userId = req.params.id
-  res.render('users/rename',{userId})
-}
+module.exports.rename = (req, res) => {
+  var userId = req.params.id;
+  res.render("users/rename", { userId });
+};
 
-module.exports.postRename = (req,res)=>{
+module.exports.postRename = (req, res) => {
   var id = req.params.id;
   var name = req.body.name;
-  db.get('users').find({id:id}).assign({ name: name})
-  .write();
-  res.redirect("/users")
-}
+  req.body.avatarUrl = req.file.path
+    .split("\\")
+    .slice(1)
+    .join("\\");
+  db.get("users")
+    .find({ id: id })
+    .assign({ name: name, avatarUrl: req.body.avatarUrl })
+    .write();
+  db.unset()
+  .write()
+  res.redirect("/users");
+};
 
-module.exports.postCreate = (req,res)=>{
+module.exports.postCreate = (req, res) => {
   req.body.id = shortid.generate();
   var errors = [];
-  if(!req.body.name ||req.body.name.length > 30)
-    errors.push("Không để trống, Số kí tự không vượt quá 30 !")
-  if(errors.length){
-    res.render('users/createUser',{
+  if (!req.body.name || req.body.name.length > 30)
+    errors.push("Không để trống, Số kí tự không vượt quá 30 !");
+  if (errors.length) {
+    res.render("users/createUser", {
       errors: errors
-    })
+    });
     return;
   }
   db.get("users")
     .push(req.body)
     .write();
   res.redirect("/users");
-}
+};
--- routes/user.route.js
+++ routes/user.route.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer'); 
 
+var upload = multer({ dest: './public/' })
+
 var userController = require('../controller/user.controller.js')
 
 router.get("/",userController.index)
@@ -12,7 +14,7 @@ router.get("/:id",userController.detele)
 
 router.get("/:id/rename",userController.rename)
 
-router.post("/:id/rename",userController.postRename)
+router.post("/:id/rename", upload.single('avatar'),userController.postRename)
 
 router.post("/create",userController.postCreate)
 
