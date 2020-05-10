afffde3f9460dfa05ff7b3acb7047b4351796f33
Glitch (hello-express) <none>
2020-05-10 02:57:35 +0000

    🍮🐢 Checkpoint ./views/layouts/common.pug:15273419/18 ./controller/cart.controller.js:15273419/363 ./controller/book.controller.js:15273419/24

--- controller/cart.controller.js
+++ controller/cart.controller.js
@@ -2,5 +2,12 @@ var db = require("../db");
 const shortid = require("shortid");
 
 module.exports.newTrans = (req,res,next)=>{
-  next();
+  var userId = req.signedCookies.userId;
+  var sessionId = req.signedCookies.sessionId;
+  var id = shortid.generate();
+  db.get('transactions').push({
+    id: id,
+    userid: userId,
+    
+  })
 }
